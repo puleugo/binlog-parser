661ce9af245fcbecd177eacd86ef00be65ae1dca
puleugo <puleugo@gmail.com>
2024-12-02 14:34:58 +0900

    feat: binlog 파싱 vervose 옵션 추가

--- binlog-parser.py
+++ binlog-parser.py
@@ -4,6 +4,8 @@ import subprocess
 # https://dev.mysql.com/doc/refman/8.4/en/mysqlbinlog.html
 # This is Row Based Binlog Converter
 # If you want to convert Statement Based Binlog, you can remove --base64-output=DECODE-ROWS
+
+
 def convert_binlog_to_sql(binlog_dir="./binlog", sql_dir="./sql"):
     # Ensure the directory exists
     if not os.path.exists(binlog_dir):
@@ -18,13 +20,14 @@ def convert_binlog_to_sql(binlog_dir="./binlog", sql_dir="./sql"):
 
             try:
                 # Run mysqlbinlog command to convert the binlog to sql
-                command = f"mysqlbinlog --base64-output=DECODE-ROWS -v {binlog_path} > {sql_path}"
+                command = f"mysqlbinlog -vv --base64-output=DECODE-ROWS -v {binlog_path} > {sql_path}"
                 subprocess.run(command, shell=True, check=True)
                 print(f"Converted '{binlog_path}' to '{sql_path}'.")
             except subprocess.CalledProcessError as e:
                 print(f"Failed to convert '{binlog_path}': {e}")
             except Exception as e:
                 print(f"Unexpected error: {e}")
+
 
 if __name__ == "__main__":
     convert_binlog_to_sql()
